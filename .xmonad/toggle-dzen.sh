493ede4ff8533855485b297338da34f264bea3d2
KeyboardFire <andy@keyboardfire.com>
2015-03-21 18:48:06 -0500

    more accurate status bar

--- .xmonad/toggle-dzen.sh
+++ .xmonad/toggle-dzen.sh
@@ -11,7 +11,7 @@ $(date '+%a. %b %d, %I:%M:%S %p') | \
 Battery: $(upower -d | grep percentage | grep -o '[0-9]*%') | \
 Volume: $(amixer -D pulse get Master | grep -o '[0-9]*%' | head -n1) | \
 Network: $(bc <<< "scale = 4; $(iwconfig 2>/dev/null | grep -o '[0-9]\+/[0-9]*')*100" | cut -c -5)%"
-        sleep 1
+        sleep 0.2
     done
     ) | dzen2 -y 100 -fn 'monospace-25' &
 fi
