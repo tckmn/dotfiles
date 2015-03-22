086464190ad25aa33c990f8ddab052d6933862cf
KeyboardFire <andy@keyboardfire.com>
2015-03-21 22:19:18 -0500

    fancy charging indicator

--- .xmonad/toggle-dzen.sh
+++ .xmonad/toggle-dzen.sh
@@ -22,6 +22,7 @@ else
     while :
     do
         battery=$(upower -d | grep percentage | grep -o '[0-9]*')
+        charging=$([ "$(upower -d | grep state | grep discharging)" ] && echo -n '' || echo -n ' ⚡')
         volume=$(amixer -D pulse get Master | grep -o '[0-9]*%' | head -n1 | grep -o '[0-9]*')
         network=$(bc -l <<< "x=$(iwconfig 2>/dev/null | grep -o '[0-9]\+/[0-9]*')*4;scale=0;x/1")
         case $network in
@@ -32,7 +33,7 @@ else
         esac
         echo "\
 $(date '+%a. %b %d, %I:%M:%S %p') | \
-^fg($(color $battery))^r($((battery))x20)^ro($((100-battery))x20)^r(5x10)^fg() | \
+^fg($(color $battery))^r($((battery))x20)^ro($((100-battery))x20)^r(5x10)$charging^fg() | \
 ^fg($(color $volume))^co(100)^p(-$((50+volume/2)))^c($volume)^p(+$((51-volume/2)))^fg() | \
 ^i($HOME/.xmonad/network-wireless-signal-$network-symbolic.xpm)"
         sleep 0.1
