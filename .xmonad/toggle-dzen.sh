a36973be7f75187c2fdd60a7f0c7a62e84c0a71c
KeyboardFire <andy@keyboardfire.com>
2015-03-21 22:09:05 -0500

    WOW VERY FANCY STATUS BAR :D

--- .xmonad/toggle-dzen.sh
+++ .xmonad/toggle-dzen.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+function color {
+    red=$(bc -l <<< "x=(100-$1)/50*255;scale=0;x/1")
+    green=$(bc -l <<< "x=$1/50*255;scale=0;x/1")
+    echo "#$(printf '%.2x%.2x' $((red>255?255:red)) $((green>255?255:green)))00"
+}
+
+# icons can be generated via:
+# cp /usr/share/icons/gnome/scalable/status/network-wireless-signal-*-symbolic.svg .; for x in *.svg; do inkscape -z -e $(basename $x .svg).png -w 32 -h 32 $x; convert $(basename $x .svg).png -background white -alpha Background $(basename $x .svg).xpm; done; rm *.svg *.png
+# I know. Very very ugly :(
+
+# Just For Fun(tm): change the line that sets battery to
+# battery=$((10#$(date +%N | cut -c 1-2)))
+# and remove the sleep 0.1.
+
 if [ "$(pidof -x toggle-dzen.sh | grep ' [^ ]* ')" ]
 then
     killall toggle-dzen.sh
@@ -6,12 +21,21 @@ else
     (
     while :
     do
+        battery=$(upower -d | grep percentage | grep -o '[0-9]*')
+        volume=$(amixer -D pulse get Master | grep -o '[0-9]*%' | head -n1 | grep -o '[0-9]*')
+        network=$(bc -l <<< "x=$(iwconfig 2>/dev/null | grep -o '[0-9]\+/[0-9]*')*4;scale=0;x/1")
+        case $network in
+            0) network=weak ;;
+            1) network=ok ;;
+            2) network=good ;;
+            3) network=excellent ;;
+        esac
         echo "\
 $(date '+%a. %b %d, %I:%M:%S %p') | \
-Battery: $(upower -d | grep percentage | grep -o '[0-9]*%') | \
-Volume: $(amixer -D pulse get Master | grep -o '[0-9]*%' | head -n1) | \
-Network: $(bc <<< "scale = 4; $(iwconfig 2>/dev/null | grep -o '[0-9]\+/[0-9]*')*100" | cut -c -5)%"
-        sleep 0.2
+^fg($(color $battery))^r($((battery))x20)^ro($((100-battery))x20)^r(5x10)^fg() | \
+^fg($(color $volume))^co(100)^p(-$((50+volume/2)))^c($volume)^p(+$((51-volume/2)))^fg() | \
+^i($HOME/.xmonad/network-wireless-signal-$network-symbolic.xpm)"
+        sleep 0.1
     done
     ) | dzen2 -y 100 -fn 'monospace-25' &
 fi
