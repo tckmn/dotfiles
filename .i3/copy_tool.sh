c03e86d8ff024b9ae191b383713772bbd2c30101
KeyboardFire <andy@keyboardfire.com>
2015-05-30 23:08:11 -0500

    copy tool

---
+++ .i3/copy_tool.sh
@@ -0,0 +1,8 @@
+echo -n "$(zenity --list --text 'Copy what?' --hide-header --column x \
+    "$(printf 'E-  Elbereth\n]')" \
+    ಠ_ಠ \
+)" \
+| perl -e 'my $s = do { local $/; <STDIN> }; $s =~ s/(.*)\|\1/\1/s; print $s' \
+| xclip -selection clipboard
+# the perl weirdness is necessary because of a zenity bug:
+# https://bugs.launchpad.net/ubuntu/+source/zenity/+bug/1267788
