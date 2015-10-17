612f9d8fc0274f150da377cff75caf6e88e76538
KeyboardFire <andy@keyboardfire.com>
2015-10-17 15:10:41 -0500

    add shrug, xclip -> xsel

--- .i3/copy_tool.sh
+++ .i3/copy_tool.sh
@@ -3,8 +3,9 @@ echo -n "$(zenity --list --text 'Copy what?' --hide-header --column x \
     ಠ_ಠ \
     '( ͡° ͜ʖ ͡°)' \
     '(╯°□°）╯︵ ┻━┻' \
+    '¯\_(ツ)_/¯' \
 )" \
 | perl -e 'my $s = do { local $/; <STDIN> }; $s =~ s/(.*)\|\1/\1/s; print $s' \
-| xclip -selection clipboard
+| xsel -ib
 # the perl weirdness is necessary because of a zenity bug:
 # https://bugs.launchpad.net/ubuntu/+source/zenity/+bug/1267788
