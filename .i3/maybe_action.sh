7f0d6eda36ec1ff58f097a6d9534fa00545d5e47
KeyboardFire <andy@keyboardfire.com>
2015-10-18 15:44:44 -0500

    made copy tool into a mode

--- .i3/maybe_action.sh
+++ .i3/maybe_action.sh
@@ -1,7 +1,7 @@
 chkfile="$HOME/.i3/__maybe_action"
 if [ -f "$chkfile" ]
 then
-    i3-msg 'mode "action: (l)aunch  (s)ystem"'
+    i3-msg 'mode "action: (l)aunch  (s)ystem  (c)opy"'
 else
     touch "$chkfile"
     sleep 0.3
