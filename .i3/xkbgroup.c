1649a35d2305f6b5fee08fbf1b8dbeedc6ff01e9
KeyboardFire <andy@keyboardfire.com>
2017-01-08 19:39:18 -0600

    i3: config overhaul
    A ton of stuff changed here, most notably the migration from i3bang to
    ppi3. But a ton of other stuff too.

--- .i3/xkbgroup.c
+++
@@ -1,7 +0,0 @@
-// compile with: gcc -lX11 xkbgroup.c -o xkbgroup
-#include <X11/XKBlib.h>
-int main() {
-    XkbStateRec xkbState;
-    XkbGetState(XkbOpenDisplay("", NULL, NULL, NULL, NULL, NULL), XkbUseCoreKbd, &xkbState);
-    return xkbState.group;
-}
--- .i3/xkbstatus.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-setxkbmap -query |
-    awk '/layout/{print $2}' |
-    awk -F, '{gsub(".*","[&]",$'$[$(~/.i3/xkbgroup;echo $?)+1]')}{print $0}'
