02fbd680fe81dfc718fdb3bc01d3b90c5d58bd4b
Andy Tockman <andy@tck.mn>
2022-07-27 00:29:58 -0400

    add ors

--- .local/share/plover/convert.rb
+++ .local/share/plover/convert.rb
@@ -66,10 +66,12 @@ rep = '²³⁴⁵⁶⁷⁸⁹'
 preg = /\(([^)]+)\)/
 qqeg = /(.)\?\?/
 qreg = /(.)\?/
+oreg = /(.)\|(.)/
 process = -> s {
     s =~ preg ? (process[s.sub(preg, '')] + process[s.sub(preg, '\1')]) :
     s =~ qqeg ? (process[s.sub(qqeg, ?-)] + process[s.sub(qqeg, '\1')]) :
     s =~ qreg ? (process[s.sub(qreg, '')] + process[s.sub(qreg, '\1')]) :
+    s =~ oreg ? (process[s.sub(oreg, '\1')] + process[s.sub(oreg, '\2')]) :
     rep.index(s[-1]) ? [([s[0..-2]] * (rep.index(s[-1]) + 2)) * ?/] :
     [s]
 }
@@ -80,11 +82,12 @@ File.write 'dict.json',
         .reject{|x| x.empty? || x[0] == ?# }
         .flat_map{|x|
             parts = x.split '; '
-            mid = parts.size/2
-            parts[0...mid].zip(parts[mid+1..-1]) + [parts[mid].split(nil, 2)]
+            a,b = parts[mid = parts.size/2].split nil, 2
+            (parts[0...mid] + [a]).zip([b] + parts[mid+1..-1])
         }
-        .flat_map{|x| a,b = x; a.split(?,).map{|a| [a,b]} }
-        .flat_map{|x| a,b = x; process[a].map{|a| [a,b]} }
+        .flat_map{|x| a,b = x; a.split(?,).map{|a| [a,b.sub(/ +# .*/, '')]} }
+        .flat_map{|x| a,b = x; groups = []; process[a.gsub(/\[([^\]]+)\]/) { groups.push $1; "#{groups.size-1}" }].map{|a| [a.gsub(/[0-9]/) {|d| groups[d.to_i] },b]} }
+        .flat_map{|x| a,b = x; groups = []; process[a.gsub(/\[([^\]]+)\]/) { groups.push $1; "#{groups.size-1}" }].map{|a| [a.gsub(/[0-9]/) {|d| groups[d.to_i] },b]} }
         .to_h
         .transform_keys{|k| k.sub(/^\\#/, ?#).split(?/).map{|s| check subs.reduce(s){|s,r| s.gsub *r}, k}*?/ }
         .to_json
